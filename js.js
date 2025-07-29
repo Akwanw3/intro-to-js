9fc4b96f4cfa820a9c542c1898fd267382972a55
ABIA AKPE <akwanwesunday@gmail.com>
2025-07-29 17:46:44 +0100

    jfdsjkhjghfgfghj

---
+++ js.js
@@ -0,0 +1,48 @@
+let num1= 123
+let num2 =84
+let sum= num1 + num2
+console.log(sum)
+let difference = num1 - num2
+console.log(difference)
+let product = num1 * num2
+console.log(product)
+let quotient = num1 / num2
+console.log(quotient)
+let remainder = num1 % num2
+console.log(remainder)
+
+let x= 5
+let summary= x+=10
+console.log(summary)
+
+let value1 = 'henry is a student'
+let value2 = 'henry has a library card'
+let value3= value1+ ' ' + value2
+console.log(value3)
+
+let firstName = 'henry'
+let lastName = 'john'
+let messsage = `my name is ${firstName} ${lastName}`
+console.log(messsage)
+let fullName = 'joy';
+
+let fruit =['apple', 'banana', 'orange', 'grape', 'kiwi']
+console.log(fruit)
+fruit[2]= 'paw-paw'
+console.log(fruit)
+fruit.push('mango')
+console.log(fruit)
+fruit.pop()
+console.log(fruit)
+fruit.unshift('strawberry')
+console.log(fruit)
+fruit.shift()
+console.log(fruit)
+
+for(let i=2; i<=20; i+=7
+    
+){
+    console.log(i)
+}
+
+
