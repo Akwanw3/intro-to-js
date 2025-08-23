551f9561a84faff3ffeab12abdc6847232762b4b
ABIA AKPE <akwanwesunday@gmail.com>
2025-08-23 08:34:55 +0100

    progressing

--- JAVASCRIPT LEARNING QUEST/script.js
+++ JAVASCRIPT LEARNING QUEST/script.js
@@ -24,32 +24,165 @@ function calculateResult(){
     let opp = document.getElementById('operation');
     let result = document.getElementById('calc-output');
     let sum = (Number(a)+Number(b));
-    let nonsense = (a==="" || b==='');
-   
-    for (i=0; i<opp.options.length; i++){
-       
-        let minus = a-b;
+    let minus = a-b;
         let times = a*b;
         let div = a/b;
         let mod = a%b;
-    let ope = opp.options[i].value;
-    if (ope==='add' && !nonsense){
-        result.textContent = `your answer is,${sum} , 😁`;
+    let nonsense = (a==="" || b==='');
+   
+    if (opp.value ==='add' && !nonsense){
+        result.textContent = `your answer is ${sum}  😁`;
         result.classList.add('success');
-    }
+    }else if (opp.value ==='subtract' && !nonsense){
+        result.textContent = `your answer is ${minus}  😁`;
+        result.classList.add('success');
+    }else if (opp.value ==='multiply' && !nonsense){
+        result.textContent = `your answer is ${times}  😁`;
+        result.classList.add('success');
+    }else if (opp.value ==='divide' && !nonsense){
+        result.textContent = `your answer is ${div}  😁`;
+        result.classList.add('success');
+    }  else if (opp.value ==='modulus' && !nonsense){
+        result.textContent = `your answer is ${mod}  😁`;
+        result.classList.add('success');      
+    }else{
+        result.textContent = `Werey, who be your math teacher  `
+         result.classList.add('error');
+        result.classList.remove('success');
 
-}
+    }
  if (a=='' && b==''){
         result.textContent = `Oga Enter a valid number no de mumu yourself`;
         result.classList.add('error');
         result.classList.remove('success');
     }
-
 }
 
 
 
+function clearCalculator(){
+   document.getElementById('num1').value ='';
+   document.getElementById('num2').value ='';
+   document.getElementById('calc-output').textContent= ''; 
+     }
 
 
-//result.textContent = 100 
 
+function exploreDataType(){
+    let inp = document.getElementById('dataInput');
+    let result= document.getElementById('type-output');
+    let input = inp.value;
+    let type ;
+    if(input ==='true' || input==="false"){
+       type = 'Boolean' ;
+    }else if (!isNaN((input)) && input.trim()!==''){
+        type = 'Number';
+    }else{
+        type = 'sring';
+    }
+
+    if (input !==''){
+       result.textContent = `The Data type you entered is a ${type}.`;
+        result.classList.add('success'); 
+    } else{
+       result.textContent = `Oga you go type something oo, No do like witch`; 
+       result.classList.add('error');
+       result.classList.remove('success');
+    }
+   
+        }
+
+
+     
+
+
+/////Arrays & Loops
+let Myarray = [];
+ let input = document.getElementById('gradeInput');
+ let result = document.getElementById('grades-output')
+function addGrade(){
+   if(input.value !==''){
+    Myarray.push(input.value);
+    input.value = '';
+   }else{
+    result.textContent = `Enter a score na, No stress me ooo`;
+    result.classList.add('error');
+    result.classList.remove('success')
+   }
+}
+
+function showAllGrades(){
+if (Myarray.length){
+    result.textContent = Myarray.join();
+    result.classList.add('success')
+}else{
+    result.textContent =`No score has been recorded `;
+    result.classList.add('error');
+    result.classList.remove('success');
+}
+}
+
+function calculateAverage(){
+    let sum = 0;
+    for (let i=0; i<Myarray.length; i++){
+        sum += Number( Myarray[i]);
+    }
+    let average = sum/Myarray.length;
+    if (Myarray.length){
+    result.textContent = average
+    result.classList.add('success')
+}else{
+    result.textContent =`No average has been recorded `;
+    result.classList.add('error');
+    result.classList.remove('success')
+}
+
+}
+
+function findHighestGrade(){
+let highest = Myarray[0];
+for (let i=0; i<Myarray.length; i++){
+    if (Myarray[i] > highest){
+        highest = Myarray[i]
+    }
+}
+if (Myarray.length){
+    result.textContent = highest
+    result.classList.add('success')
+}else{
+    result.textContent =`No de mumu yourself for here oo `;
+    result.classList.add('error');
+    result.classList.remove('success');
+}
+
+}
+
+function findFailingGrades(){
+    let lowscr = Myarray.filter(grade => grade<60);
+    if (Myarray.length){
+    result.textContent = lowscr.join();
+    result.classList.add('success')
+}else{
+    result.textContent =`I go soon beat you `;
+    result.classList.add('error');
+    result.classList.remove('success')
+}
+}
+
+
+function removeLastGrade(){
+     if(Myarray.length){
+    Myarray.pop();
+   }else{
+    result.textContent = `No click nonsense for here again!!!`;
+    result.classList.add('error');
+    result.classList.remove('success');
+   }
+}
+
+function clearAllGrades(){
+    Myarray.length=0;
+    result.textContent = ''
+    input.value = ''
+}
+ 
